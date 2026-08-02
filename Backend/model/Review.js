a2d263548ea0b9bc4afacd6686b2e84f28c8e1c8
Sarath Dharmaraj <sarathdharmaraj0024@gmail.com>
2026-08-02 14:59:54 +0530

    New review model added

---
+++ Backend/model/Review.js
@@ -0,0 +1,30 @@
+import mongoose, { Mongoose } from "mongoose";
+
+const reviewSchema = new mongoose.Schema(
+  {
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+      min: [1, "Rating must be atleast 1"],
+      man: [5, "Rating must be atmost 5"],
+    },
+    comment: {
+      type: String,
+    },
+  },
+  { timestamps: true },
+);
+
+reviewSchema.index({ productId: 1, userId: 1 }, { unique: true });
+
+export default mongoose.model("Review", reviewSchema);
